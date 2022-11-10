a2c3897f91709b748e84b911d44f188d6fe52f46
Nedoxy <nk.nedum@gmail.com>
2022-11-10 19:13:26 +0100

    Question 5

--- Question_5.py
+++ Question_5.py
@@ -1,30 +1,14 @@
-
 """Rite  a  Python  program  that  checks  if  a  string  is  a palindrome,  Then  optionally  write  a  unit  test
 to  check your program's correctness. """
 """A palindrome is a string that reads the same forwards and backwards."""
 
 
-def palindrome_check(main_list, user_string):
-    # reversing the list
-    inverse_list = main_list[::-1]
-
-    # comparing reversed_list to original_list to
-    # determine if the original_list is a palindrome
-    if main_list == inverse_list:
-        print("'" + user_string.lower() + "' is a palindrome.")
+def is_a_palindrome(string):
+    if string == string[::-1]:
+        print("Is Palindrome")
     else:
-        print("'" + user_string.lower() + "' is not a palindrome!")
+        print("Not a Palindrome")
 
 
-def main():
-    # promting user to input a word or phrase
-    user_string = input("Enter string: Madam ")
-
-    user_list = list(user_string.lower())
-
-    # putting our palindrome_function() to use
-    palindrome_check(user_list, user_string)
-
-
-if __name__ == "__main__":
-    main()
+is_a_palindrome("Mama")
+is_a_palindrome("rotator")
