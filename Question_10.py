bd5323c5d269b28dfba839f3ab8fef92ed3661bb
Nedoxy <nk.nedum@gmail.com>
2022-11-10 18:14:27 +0100

    Question 10

---
+++ Question_10.py
@@ -0,0 +1,8 @@
+"""Create a function that converts any string value to aSentence  case,  Then  write  a  unit  test  that  checks  thefunction's correctness.TECHNICAL QUESTIONS
+"""
+
+
+club = "fcbarcelona"
+
+sentence_case = club.title()
+print(sentence_case)
