11c5bf2efdaa2d800b0aa1414e90a15749904587
Nedoxy <nk.nedum@gmail.com>
2022-11-10 16:52:51 +0100

    Question 2

--- Question_2.py
+++ Question_2.py
@@ -1,5 +1,5 @@
 """ Create  a  function  that  calculates  the  power  of  a  number.
-Then  write  a  unit  test  to  check  the  correctness  of  the
+Then  write  a unit  test  to  check  the  correctness  of  the
 function.
 """
 
