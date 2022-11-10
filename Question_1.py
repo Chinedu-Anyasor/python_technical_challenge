1b602fb52a998ae1ace08f7963d253db6301fca9
Nedoxy <nk.nedum@gmail.com>
2022-11-10 16:50:51 +0100

    Question 1

--- Question_1.py
+++ Question_1.py
@@ -1,4 +1,4 @@
-"""Create  a  two-string  variable,  concatenate  the  two  variables and print out the result."""
+"""Create  a  two-string  variable, concatenate  the  two  variables and print out the result."""
 
 FirstName = "Chinedu"
 LastName = "Anyasor"
