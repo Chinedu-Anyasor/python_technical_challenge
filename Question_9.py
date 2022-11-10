f3fe3b7a005e117a54eb5c9d3099035d1fa0df1c
Nedoxy <nk.nedum@gmail.com>
2022-11-10 18:14:01 +0100

    Question 9

---
+++ Question_9.py
@@ -0,0 +1,8 @@
+
+""" Create  a  function  that  converts  any  string  value  to  uppercase,  Then  write  a  unit  test  that  checks
+ the  function's correctness """
+
+club = "fcbarcelona"
+
+upper_case = club.upper()
+print(upper_case)
